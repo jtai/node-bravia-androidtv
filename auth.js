ffb9c6275f97f4f1f5e290cbc57b02e991b45c5c
Jon Tai <jon.tai@gmail.com>
2016-10-13 00:04:50 -0700

    Simplify auth.js, call rl.close() to fix hang

--- auth.js
+++ auth.js
@@ -4,34 +4,37 @@ var Bravia = require('./bravia');
 
 var bravia = new Bravia();
 
-bravia.auth()
-  .then(function(response) {
-    var deferred = Q.defer();
+bravia.auth().then(function(response) {
+  if (response.statusCode == 200) {
+    console.log('Already registered!');
+    console.log(response.headers);
+    return;
+  }
 
-    if (response.statusCode == 401) {
-      var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+  if (response.statusCode == 401) {
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('Please enter the 4-digit code shown on your TV: ', function(code) {
+      bravia.auth(code).then(function(response) {
+        if (response.statusCode == 200) {
+          console.log('Registered!');
+          console.log(response.headers);
+        } else {
+          console.log('Unexpected '+response.statusCode+' response');
+        }
+      }, function(error) {
+        console.log(error);
       });
 
-      rl.question('Please enter the 4-digit code shown on your TV: ', function(code) {
-        deferred.resolve(code);
-      });
-    } else {
-      console.log(response.headers);
-      deferred.reject('Unexpected '+response.statusCode+' response starting auth');
-    }
+      rl.close();
+    });
+    return;
+  }
 
-    return deferred.promise;
-  })
-  .then(function(code) {
-    return bravia.auth(code);
-  })
-  .then(function(response) {
-    if (response.statusCode == 200) {
-      console.log(response.headers);
-    }
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+  console.log('Unexpected '+response.statusCode+' response');
+}, function(error) {
+  console.log(error);
+});
